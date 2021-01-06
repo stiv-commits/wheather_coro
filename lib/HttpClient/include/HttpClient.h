0f41394967fcb6c14851592f9b551c609e1b290c
stiv-commits <stiv845@gmail.com>
2021-01-06 12:09:09 +0300

    Релиз HttpClient D stiv-commits

--- lib/HttpClient/include/HttpClient.h
+++ lib/HttpClient/include/HttpClient.h
@@ -1,64 +1,57 @@
 #pragma once
 
-#include <queue>
+#include <deque>
+#include <thread>
+#include <memory>
 #include "cfg.h"
-#include <boost/asio/ip/tcp.hpp>
-#include <boost/asio/spawn.hpp>
-#include <boost/asio/deadline_timer.hpp>
 #include <boost/beast/http.hpp>
 #include <boost/beast/core.hpp>
-#include <boost/asio/ssl/error.hpp>
-#include <boost/asio/ssl/stream.hpp>
-
-//class HttpSession;
-//using HttpSessionPtr = std::shared_ptr<HttpSession>;
+#include <boost/asio/co_spawn.hpp>
+#include <boost/asio/io_context.hpp>
 
 
+namespace httpclient
+{
+  class HttpClient;
+  using HttpClientPtr = std::shared_ptr<HttpClient>;
 
-//class HttpsClient
-//{
-//public:
-//  struct HttpsCfg
-//  {
-//    std::string url;
-//    std::string port;
-//  };
-//
-//  HttpsClient(const HttpsCfg& cfg, boost::asio::io_context& ioc);
-//
-//
-//  void Stop();
-//
-//  boost::beast::http::response<boost::beast::http::dynamic_body> send_recv_async(boost::asio::yield_context& parentYield,
-//    boost::beast::http::request<boost::beast::http::string_body> req);
-//
-//private:
-//
-//  bool Start(boost::asio::yield_context yld);
-//  bool Connect(boost::asio::yield_context& yld);
-//  void Disconnect(boost::asio::yield_context& yld);
-//  bool OnSendRecvError(const boost::system::error_code& err, boost::asio::yield_context& yld);
-//
-//  struct ReqAsync
-//  {
-//    ReqAsync(boost::asio::io_context& ioc)
-//      :wakeupTimer(ioc)
-//    {}
-//    ~ReqAsync() = default;
-//
-//    boost::asio::deadline_timer wakeupTimer;
-//    boost::beast::http::request<boost::beast::http::string_body> req;
-//    boost::beast::http::response<boost::beast::http::dynamic_body> res;
-//  };
-//  using ReqAsyncPtr = std::shared_ptr<ReqAsync>;
-//
-//  //  callback cb_;
-//  HttpsCfg clientCfg_;
-//  boost::asio::io_context& ioc_;
-//  std::deque<ReqAsyncPtr> data_;
-//  std::unique_ptr<boost::asio::deadline_timer> timer_;
-//  bool fStop = false;
-//  // boost::beast::flat_buffer b;
-//  boost::asio::ssl::context ctx_{ boost::asio::ssl::context::sslv23_client };
-//  boost::asio::ssl::stream<boost::asio::ip::tcp::socket> stream_;
-//};
+  class ManagerConn
+  {
+  public:
+    struct Dependencies
+    {
+      Dependencies(boost::asio::io_context& ioc_)
+        :ioc(ioc_)
+      {}
+
+      boost::asio::io_context& ioc;
+    };
+
+    struct Config
+    {
+      uint8_t numConn = 1;
+      std::string url;
+      std::string port;
+    };
+
+    ManagerConn(const Config& cfg, const Dependencies& dep);
+    void Init();
+    boost::asio::awaitable<boost::beast::http::response<boost::beast::http::dynamic_body>>
+      SendRecvAsync(const boost::beast::http::request<boost::beast::http::string_body>& req);
+    void Stop();
+
+  private:
+
+    boost::asio::awaitable<HttpClientPtr> GetClient();
+    boost::asio::awaitable<void> Reconnect(HttpClientPtr httpClient);
+    boost::asio::awaitable<void> Connect(HttpClientPtr httpClientArg);
+    boost::asio::awaitable<void> Disconnect(HttpClientPtr httpClient);
+
+    Config cfg_;
+    Dependencies dep_;
+
+    std::mutex mut_;
+    std::deque<HttpClientPtr> httpClients_;
+    std::atomic_bool fEnd_ = false;
+  };
+}
