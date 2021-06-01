d8339a8987a7d79d4e66c660fe0f9b842ca28402
stiv-commits <stiv845@gmail.com>
2021-06-01 14:21:19 +0300

    Added rtree storage for coordinate

--- lib/InMemoryDb/src/InMemoryDb.cpp
+++ lib/InMemoryDb/src/InMemoryDb.cpp
@@ -92,19 +92,87 @@ private:
 
 };
 
-class InMemoryDb::WeatherByCoorStorage // TODO Must implemente RTree
+namespace bg = boost::geometry;
+namespace bgm = bg::model;
+namespace bgi = bg::index;
+
+typedef bg::model::point<double, 2, bg::cs::cartesian> point;
+
+struct WheatheRef {
+  Weather weather;
+  Coordinate coor;
+  time_t time;
+  point location;
+};
+
+template <>
+struct bgi::indexable<WheatheRef>
 {
+  typedef point result_type;
+  point operator()(const WheatheRef& c) const { return c.location; }
+};
+
+class InMemoryDb::WeatherByCoorStorage
+{
+
 public:
 
-  Weather GetWeatherByCoor(const Coordinate& adr, uint32_t timeLeftSec, uint32_t distanceMetr)
+  Weather GetWeatherByCoor(const Coordinate& coor, uint32_t timeLeftSec, uint32_t distanceMetr)
   {
+    point pt(coor.lat, coor.lon);
+
+    auto it = rtree_.qbegin(bgi::nearest(pt, 1000));
+    if (it != rtree_.qend())
+    {
+      auto dist = CalcGPSDistance(it->coor.lat, it->coor.lon, coor.lat, coor.lon);
+
+      if (dist > distanceMetr) return Weather{};
+
+      if (time(nullptr) > it->time + timeLeftSec) return Weather{};
+
+      return it->weather;
+    }
+
     return Weather{};
   }
+
   void PutWeather(const Coordinate& coor, const Weather& weather)
   {
-
+    WheatheRef we;
+    we.location = { coor.lat, coor.lon };
+    we.weather = weather;
+    we.coor = coor;
+    we.time = time(nullptr);
+    rtree_.insert(std::move(we));
   }
+
 private:
+
+  const double PI = 3.14159265358979323846;
+  const double RADIO_TERRESTRE = 6372797.56085;
+  const double GRADOS_RADIANES = PI / 180;
+
+  float CalcGPSDistance(float latitud1, float longitud1, float latitud2, float longitud2) {
+    double haversine;
+    double temp;
+    double distancia_puntos;
+    
+    using namespace std;
+
+    auto latitud1Dob = latitud1 * GRADOS_RADIANES;
+    auto longitud1Dob = longitud1 * GRADOS_RADIANES;
+    auto latitud2Dob = latitud2 * GRADOS_RADIANES;
+    auto longitud2Dob = longitud2 * GRADOS_RADIANES;
+
+    haversine = (pow(sin((1.0 / 2) * (latitud2Dob - latitud1Dob)), 2)) + ((cos(latitud1Dob)) * (cos(latitud2Dob)) * (pow(sin((1.0 / 2) * (longitud2Dob - longitud1Dob)), 2)));
+    temp = 2 * asin(min(1.0, sqrt(haversine)));
+    distancia_puntos = RADIO_TERRESTRE * temp;
+
+    return static_cast<float>(distancia_puntos);
+  }
+
+  typedef bgi::rtree< WheatheRef, bgi::linear<16> > rtree_t;
+  rtree_t rtree_;
 
 };
 
@@ -139,4 +207,5 @@ Weather InMemoryDb::GetWeatherByCoor(const Coordinate& adr, uint32_t timeLeftSec
 
 void InMemoryDb::PutWeather(const Coordinate& coor, const Weather& weather)
 {
+  return weatherByCoorStorage_->PutWeather(coor, weather);
 }
